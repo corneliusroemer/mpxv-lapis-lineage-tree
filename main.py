9376ceffe71c86d6b090b9ff082bf63a0a6db65b
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-12-02 16:09:03 +0100

    First working version

---
+++ main.py
@@ -0,0 +1,60 @@
+# %%
+# Clone the repository to ./lineage-designations/
+import os
+import pathlib
+from typing import Any
+
+import yaml
+from git import Repo
+
+# %%
+
+REPO_URL = "https://github.com/mpxv-lineages/lineage-designation"
+LOCAL_DIR = "./lineage-designation/"
+repo = Repo.clone_from(REPO_URL, LOCAL_DIR)
+
+# %%
+
+# Folder containing lineage YAML files
+# input_folder = LOCAL_DIR / "lineages"
+input_folder = pathlib.Path(LOCAL_DIR) / "lineages"
+output_file = "lineage_dag.yaml"
+
+# Dictionary to store the DAG
+lineage_dag = {}
+
+# Iterate through each YAML file in the folder
+for filename in os.listdir(input_folder):
+    if filename.endswith(".yml") or filename.endswith(".yaml"):
+        filepath = os.path.join(input_folder, filename)
+        with open(filepath, "r") as file:
+            lineage_data: dict[str, Any] = yaml.safe_load(file)
+            name = lineage_data.get("name")
+            parent = lineage_data.get("parent")
+            alias = lineage_data.get("alias")
+            unaliased_name = lineage_data.get("unaliased_name")
+
+            # Ensure name entry exists in DAG
+            if name not in lineage_dag:
+                lineage_dag[name] = {
+                    "parents": [],
+                    "aliases": [],
+                }
+
+            # Add parent if available
+            if parent:
+                lineage_dag[name]["parents"].append(parent)
+
+            # Add aliases
+            if alias:
+                lineage_dag[name]["aliases"].append(alias)
+            if unaliased_name and unaliased_name != name:
+                lineage_dag[name]["aliases"].append(unaliased_name)
+
+# Write the DAG to a consolidated YAML file
+with open(output_file, "w") as output:
+    yaml.dump(lineage_dag, output, default_flow_style=False)
+
+print(f"Lineage DAG written to {output_file}")
+
+# %%
